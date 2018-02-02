9bf8b7ff361b3501c0f83a4254e9986c4486bc55
gabarne <gabarne@sandia.gov>
2018-02-02 13:43:53 -0700

    Organized version of nonhydro model is now working Eulerian style.

--- COMPOSE/nonhydroXZ/rbfSemiLagrangian/rbfSemiLagrangian.py
+++ COMPOSE/nonhydroXZ/rbfSemiLagrangian/rbfSemiLagrangian.py
@@ -2,27 +2,29 @@ import sys
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from gab import nonhydro, phs2
+from gab import nonhydro, phs2, rk
 
 ###########################################################################
 
 testCase = "doubleDensityCurrent"
 formulation = "exner"
 semiLagrangian = 0
-dx = 400.
-ds = 400.
-FD = 4                              #positive even number
-plotFromSaved = 0
+dx = 100.
+ds = 100.
+FD = 4                          #Order of lateral FD (positive even number)
 rbforder = 3
 polyorder = 1
 stencilSize = 9
+saveDel = 50
+var = 3
+plotFromSaved = 1
 
 ###########################################################################
 
 t = 0.
 
-saveString = './results/' + testCase + '/dx' + \
-'{0:1d}'.format(np.int(dx)) + 'ds' + '{0:1d}'.format(np.int(ds)) + '/'
+saveString = './results/' + testCase + '/dx' \
++ '{0:1d}'.format(np.int(dx)) + 'ds' + '{0:1d}'.format(np.int(ds)) + '/'
 
 ###########################################################################
 
@@ -35,10 +37,16 @@ tf, dt, nTimesteps = nonhydro.getTimeDomain( testCase, dx, ds )
 
 s, dsdx, dsdz = nonhydro.getHeightCoordinate( zTop, zSurf, zSurfPrime )
 
-FDo2 = np.int( FD/2 )
 ii = np.arange( 1, nLev+1 )
+FDo2 = np.int( FD/2 )
 jj = np.arange( FDo2, nCol+FDo2 )
-Tx, Tz, Nx, Nz = nonhydro.getTanNorm( nCol, zSurfPrime, x[0,jj] )
+
+i0 = ii[0]
+i1 = ii[-1] + 1
+j0 = jj[0]
+j1 = jj[-1] + 1
+
+Tx, Tz, Nx, Nz = nonhydro.getTanNorm( zSurfPrime, x[0,jj] )
 
 U, thetaBar, piBar = nonhydro.getInitialConditions( testCase, formulation \
 , nLev, nCol, FD, x, z \
@@ -97,7 +105,7 @@ def setGhostNodes( U ) :
     - (U[2,nLev-1,jj]-thetaBar[nLev-1,jj])
     
     #get pi on bottom ghost nodes using derived BC:
-    dpidx = nonhydro.Lx( U[3,1:3,:], wx, jj, dx, FD )
+    dpidx = nonhydro.Lx( U[3,1:3,:], wx, jj, dx, FD, FDo2 )
     dpidx = 3./2.*dpidx[0,:] - 1./2.*dpidx[1,:]
     th = ( U[2,0,jj] + U[2,1,jj] ) / 2.
     U[3,0,jj] = U[3,1,jj] + ds/normGradS**2 * ( g/Cp/th*dsdzBottom + dpidx*dsdxBottom )
@@ -116,48 +124,97 @@ def setGhostNodes( U ) :
 
 def odefun( t, U ) :
     
-    #initialize output array:
+    #Initialize output array:
     V = np.zeros( np.shape(U) )
     
-    #set ghost node values for all variables:
+    #Set ghost node values for all variables:
     U = setGhostNodes( U )
     
-    #get Us and Ux:
+    #Us and Ux:
     Us = nonhydro.Ls( U[:,:,jj], ws, ii, ds )
-    Ux = nonhydro.Lx( U[:,ii,:], wx, jj, dx, FD )
+    Ux = nonhydro.Lx( U[:,ii,:], wx, jj, dx, FD, FDo2 )
     
-    #get u and sDot:
+    #Get u and sDot:
     u = np.tile( U[0,ii,:][:,jj], (4,1,1) )
     sDot = U[0,ii,:][:,jj] * dsdx + U[1,ii,:][:,jj] * dsdz
     sDot = np.tile( sDot, (4,1,1) )
     
-    #get RHS of ode function:
-    V[:,ii,:][:,:,jj] = - u * Ux - sDot * Us
-    V[0,ii,:][:,jj] = V[0,ii,:][:,jj] \
-    - Cp*U[2,ii,:][:,jj] * ( Ux[3,:,:] + Us[3,:,:]*dsdx )
-    V[1,ii,:][:,jj] = V[1,ii,:][:,jj] \
-    - Cp*U[2,ii,:][:,jj] * ( Us[3,:,:]*dsdz ) - g
-    V[3,ii,:][:,jj] = V[3,ii,:][:,jj] \
-    - Rd/Cv*U[3,ii,:][:,jj] \
-    * ( Ux[0,:,:]+Us[0,:,:]*dsdx + Us[1,:,:]*dsdz )
+    #RHS of ODE function:
+    V[ :, i0:i1, j0:j1 ] = - u * Ux - sDot * Us
+    V[ 0, i0:i1, j0:j1 ] = V[0,ii,:][:,jj] - Cp*U[2,ii,:][:,jj] * ( Ux[3,:,:] + Us[3,:,:]*dsdx )
+    V[ 1, i0:i1, j0:j1 ] = V[1,ii,:][:,jj] - Cp*U[2,ii,:][:,jj] * ( Us[3,:,:]*dsdz ) - g
+    V[ 3, i0:i1, j0:j1 ] = V[3,ii,:][:,jj] - Rd/Cv*U[3,ii,:][:,jj] * ( Ux[0,:,:]+Us[0,:,:]*dsdx + Us[1,:,:]*dsdz )
+    
+    #Add dissipation:
     if FD == 2 :
-        V[:,ii,:][:,:,jj] = V[:,ii,:][:,:,jj] \
-        + (1./2.) * np.abs(u) * nonhydro.Lx( U[:,ii,:], wxhv, jj, dx, FD )
+        V[ :, i0:i1, j0:j1 ] = V[:,ii,:][:,:,jj] \
+        + (1./2.) * np.abs(u) * nonhydro.Lx( U[:,ii,:], wxhv, jj, dx, FD, FDo2 )
     elif FD == 4 :
-        V[:,ii,:][:,:,jj] = V[:,ii,:][:,:,jj] \
-        - (1./12.) * np.abs(u) * nonhydro.Lx( U[:,ii,:], wxhv, jj, dx, FD )
+        V[ :, i0:i1, j0:j1 ] = V[:,ii,:][:,:,jj] \
+        - (1./12.) * np.abs(u) * nonhydro.Lx( U[:,ii,:], wxhv, jj, dx, FD, FDo2 )
     else :
         sys.exit( "\nError: FD should be 2 or 4.\n" )
-    V[:,ii,:][:,:,jj] = V[:,ii,:][:,:,jj] \
+    V[ :, i0:i1, j0:j1 ] = V[:,ii,:][:,:,jj] \
     + (1./2.) * np.abs(sDot) * nonhydro.Ls( U[:,:,jj], wshv, ii, ds )
     
     return V
 
 ###########################################################################
 
-# U = odefun( t, U )
+#Time-stepping:
 
-# sys.exit("\nStop here for now.\n")
+if testCase == "densityCurrent" :
+    fig = plt.figure( figsize = (30,10) )
+    CL = np.arange( -16.5, 1.5, 1. )
+elif testCase == "doubleDensityCurrent" :
+    fig = plt.figure( figsize = (30,12) )
+    CL = np.arange( -16.5, 1.5, 1. )
+elif testCase == "movingDensityCurrent" :
+    fig = plt.figure( figsize = (30,10) )
+    CL = np.arange( -16.5, 1.5, 1. )
+elif testCase == "bubble" :
+    fig = plt.figure( figsize = (18,14) )
+    CL = np.arange( -.05, 2.15, .1 )
+elif testCase == "igw" :
+    fig = plt.figure( figsize = (40,4) )
+    CL = np.arange( -.0021, .0035, .0002 )
+else :
+    sys.exit( "\nError: Invalid test case string.\n" )
+
+for i in range(nTimesteps+1) :
+    if np.mod( i, np.int(np.round(saveDel/dt)) ) == 0 :
+        if plotFromSaved == 0 :
+            U = setGhostNodes( U )
+            np.save( saveString+'{0:04d}'.format(np.int(np.round(t)))+'.npy', U )
+        elif plotFromSaved == 1 :
+            U = np.load( saveString+'{0:04d}'.format(np.int(np.round(t)))+'.npy' )
+        else :
+            sys.exit( "\nError: plotFromSaved should be 0 or 1.\n" )
+        if var == 0 :
+            plt.contourf( x, z, U[0,:,:], 20 )
+        elif var == 1 :
+            plt.contourf( x, z, U[1,:,:], 20 )
+        elif var == 2 :
+            plt.contourf( x, z, U[2,:,:]-thetaBar, CL )
+        elif var == 3 :
+            plt.contourf( x, z, U[3,:,:]-piBar, 20 )
+        else :
+            sys.exit( "\nError: var should be 0, 1, 2, or 3.\n" )
+        if testCase != "igw" :
+            plt.axis( 'equal' )
+        plt.colorbar()
+        plt.title( 'testCase = {0}, t = {1:04d}'.format(testCase,np.int(t)) )
+        fig.savefig( 'foo' + '{0:1d}'.format(np.int(np.round(t))) + '.png', bbox_inches = 'tight' )
+        plt.clf()
+    if plotFromSaved == 0 :
+        U = rk.rk3( t, U, odefun, dt )
+    t = t + dt
+
+###########################################################################
+
+sys.exit("\nStop here for now.\n")
+
+###########################################################################
 
 #Go from 3D array to 2D array:
 U = np.transpose( np.reshape( U, ( 4, (nLev+2)*(nCol+FD) ) ) )
@@ -168,6 +225,12 @@ U = np.reshape( np.transpose(U), ( 4, nLev+2, nCol+FD ) )
 
 #Print some things:
 
+print()
+print("dx =",dx)
+print()
+print("ds =",ds)
+print()
+print("dt =",dt)
 print()
 print("xLeft =",xLeft)
 print()
@@ -185,18 +248,23 @@ print("zSurfPrime =",zSurfPrime)
 print()
 print("tf =",tf)
 print()
-print("dt =",dt)
-print()
 print("nTimesteps =",nTimesteps)
 print()
 print(s)
 print()
-print(dsdx)
-print()
-print(dsdz)
-print()
 print("sizeU =",np.shape(U))
 print()
+
+###########################################################################
+
+#Contour plot of something:
+
+plt.contourf( x[ii,:], z[ii,:], nonhydro.Ls( U[2,:,:]-thetaBar, ws, ii, ds ) )
+if testCase != "igw" :
+    plt.axis( 'equal' )
+plt.colorbar()
+fig.savefig( 'foo' + '.png', bbox_inches = 'tight' )
+plt.clf()
 
 ###########################################################################
 
@@ -213,24 +281,12 @@ plt.plot( x[ind.gl], z[ind.gl], "o", color="red",    fillstyle="none", markersiz
 plt.plot( x[ind.r],  z[ind.r],  "s", color="red",    fillstyle="none", markersize=ms )
 plt.plot( x[ind.gr], z[ind.gr], "o", color="blue",   fillstyle="none", markersize=ms )
 plt.plot( x[ind.l],  z[ind.l],  "s", color="blue",   fillstyle="none", markersize=ms )
-plt.plot( x[ind.gb], z[ind.gb], "^", color="yellow", fillstyle="none", markersize=ms )
-plt.plot( x[ind.b],  z[ind.b],  "v", color="yellow", fillstyle="none", markersize=ms )
+plt.plot( x[ind.gb], z[ind.gb], "^", color="orange", fillstyle="none", markersize=ms )
+plt.plot( x[ind.b],  z[ind.b],  "v", color="orange", fillstyle="none", markersize=ms )
 plt.plot( x[ind.gt], z[ind.gt], "v", color="green",  fillstyle="none", markersize=ms )
 plt.plot( x[ind.t],  z[ind.t],  "^", color="green",  fillstyle="none", markersize=ms )
-plt.axis( 'equal' )
+if testCase != "igw" :
+    plt.axis( 'equal' )
 plt.show()
 
 ############################################################################
-
-#Contour plot of potential temperature field:
-
-x = np.reshape( x, (nLev+2,nCol+FD) )
-z = np.reshape( z, (nLev+2,nCol+FD) )
-
-fig = plt.figure( figsize = (25,10) )
-plt.contourf( x, z, U[2,:,:] )
-plt.axis( 'equal' )
-plt.colorbar()
-fig.savefig( 'foo' + '.png', bbox_inches = 'tight' )
-
-###########################################################################
