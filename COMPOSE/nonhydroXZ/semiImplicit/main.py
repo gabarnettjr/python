a10bc879c5e1ca5df7232c7186abbb1a3deca334
gabarne <gabarne@sandia.gov>
2018-02-26 17:06:09 -0700

    Finally found the bug, and it is working with explicit rk3 now.  Should be in the right form to make the jump to semi-implicit with minor adjustments.

--- COMPOSE/nonhydroXZ/semiImplicit/main.py
+++ COMPOSE/nonhydroXZ/semiImplicit/main.py
@@ -11,22 +11,31 @@ from gab.nonhydro import setFigAndContourLevels
 
 ###########################################################################
 
-testCase = "doubleDensityCurrent"
-dx = 100.
-dz = 100.
-FD = 4
-dt = 1./6.
+testCase = "igw"
+dx       = 500.
+dz       = 500.
+FD       = 4
+dt       = 1./2.
+saveDel  = 100
 
+saveArrays  = 0
+savePlots   = 1
+var         = 3
 plotNodes   = 0
 spyMatrices = 0
 
-saveDel = 50
+###########################################################################
+
+t = 0.
+
+saveString = './results/' + testCase + '/' \
++ 'dx' + '{0:1d}'.format(np.int(np.round(dx)+1e-12)) \
++ 'dz' + '{0:1d}'.format(np.int(np.round(dz)+1e-12)) + '/'
 
 ###########################################################################
 
 Cp, Cv, Rd, g = semiImplicit.getConstants()
 
-t = 0.
 tf = semiImplicit.getTfinal( testCase )
 nTimesteps = np.int( np.round(tf/dt) + 1e-12 )
 
@@ -34,55 +43,70 @@ xLeft, xRight, zTop, nLev, nCol, N, xx, zz, x, z \
 = semiImplicit.getSpaceDomain( testCase, dx, dz )
 
 if plotNodes == 1 :
-    plt.plot(x,z,'.')
-    plt.axis('equal')
+    plt.plot( x, z, '.' )
+    plt.axis( 'equal' )
     plt.show()
-
-Lx, Lz, HVx, HVz, gamma = semiImplicit.getDerivativeOperators( nCol, nLev \
-, FD, dx, dz )
 
 U, thetaBar, piBar, dthetabarDz, dpibarDz \
 = semiImplicit.getInitialConditions( testCase, nLev, nCol, x, z \
 , Cp, Cv, Rd, g )
 
-###########################################################################
+Lx, Lz, HVx, HVz, gamma = semiImplicit.getDerivativeOperators( nCol, nLev \
+, FD, dx, dz )
 
 Bc1, Bc2 = semiImplicit.getBoundaryConditionMatrices( thetaBar \
 , nLev, nCol, g, Cp )
+
+###########################################################################
+
+# fig, CL = setFigAndContourLevels( testCase )
+# tmp = Lz.u.dot( (z-3200.)**2. )
+# exact = 2.*(z-3200.)
+# # tmp = Lx.dot( 3200.*np.sin(np.pi*x/3200) )
+# # exact = np.pi * np.cos(np.pi*x/3200.)
+# tmp = exact - tmp
+# print()
+# print([np.min(tmp),np.max(tmp)])
+# tmp = np.reshape( tmp, (nCol,nLev) )
+# tmp = np.transpose( tmp )
+# plt.contourf( xx, zz, tmp )
+# # plt.axis( 'equal' )
+# plt.colorbar()
+# plt.savefig( '{0:04d}'.format(np.int(np.round(t)+1e-12))+'.png', bbox_inches = 'tight' )
+# plt.clf()
+
+###########################################################################
 
 A = semiImplicit.getBlockMatrix( Lx, Lz, Bc1, nLev, nCol \
 , thetaBar, piBar \
 , Cp, Cv, Rd )
 
 if spyMatrices == 1 :
-    plt.spy(A)
+    plt.spy(Lx)
     plt.show()
+    sys.exit("\nStop here for now.\n")
 
 ###########################################################################
 
-def odeFun( t, U ) :
+V = np.zeros(( 4*N ))
+
+def odeFunction( t, U ) :
     return semiImplicit.odeFun( t, U \
     , Lx, Lz, HVx, HVz, Bc1, Bc2 \
     , thetaBar, piBar, dthetabarDz, dpibarDz \
-    , N, gamma, Rd, Cv, Cp, g ) 
+    , N, gamma, Rd, Cv, Cp, g, V )
 
 def rungeKutta( t, U ) :
-    return rk.rk3( t, U, odeFun, dt )
-
-###########################################################################
+    t, U = rk.rk3( t, U, odeFunction, dt )
+    return t, U
 
 fig, CL = setFigAndContourLevels( testCase )
 
-tmp = ( U[1*N:2*N] )
-print()
-print([np.min(tmp),np.max(tmp)])
-tmp = np.reshape( tmp, (nCol,nLev) )
-tmp = np.transpose( tmp )
-plt.contourf( xx, zz, tmp )
-plt.axis( 'equal' )
-plt.colorbar()
-fig.savefig( '{0:04d}'.format(np.int(np.round(t)+1e-12))+'.png', bbox_inches = 'tight' )
-plt.clf()
+def saveContourPlot( U, t ) :
+    semiImplicit.saveContourPlot( U, t \
+    , testCase, var, nLev, nCol, N \
+    , xx, zz, CL \
+    , xLeft, xRight, zTop, dx, dz )
 
 ###########################################################################
 
@@ -91,10 +115,17 @@ et = semiImplicit.printInfo( U, time.clock(), t, N )
 for i in range(nTimesteps+1) :
 
     if np.mod( i, np.int(np.round(saveDel/dt)) ) == 0 :
-        semiImplicit.printInfo( U, et , t, N )
-        time.sleep(1)
+        if saveArrays == 1 :
+            np.save( saveString+'{0:04d}'.format(np.int(np.round(t)))+'.npy', U )
+        else :
+            U = np.load( saveString+'{0:04d}'.format(np.int(np.round(t)))+'.npy' )
+        et = semiImplicit.printInfo( U, et, t, N )
+        if savePlots == 1 :
+            saveContourPlot( U, t )
     
-    U = rungeKutta( t, U )
-    t = t + dt
+    if saveArrays == 1 :
+        t, U = rungeKutta( t, U )
+    else :
+        t = t + dt
 
 ###########################################################################
