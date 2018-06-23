1ae6e5f8b7bc39377c3593cd528dcc6360b1ca24
gabarnettjr <gabarnettjr@gmail.com>
2018-06-23 02:11:08 -0600

    Learning a little bit about batch scripts so that I can write an equivalent version of init.sh which will work in windows.

--- batch/learningBatch.bat
+++ batch/learningBatch.bat
@@ -1,6 +1,6 @@
 @echo off
 
-setlocal enableextensions enabledelayedexpansion
+REM setlocal enableextensions enabledelayedexpansion
 
 set /a "x = 5"
 set /a "y = 7"
@@ -14,17 +14,19 @@ echo[
 REM a while loop:
 set /a "x = 0"
 :while1
-    if %x% leq 5 (
-        echo %x%
-        set /a "x = x + 1"
-        goto :while1
-    )
+if %x% leq 5 (
+    echo %x%
+    set /a "x = %x% + 1"
+    goto :while1
+)
 
 echo[
 
 REM a for loop:
 for /l %%i in (1,1,5) do (
     echo i = %%i
+    set /a "x = %x% - 1"
+    echo x = %x%
 )
 
-endlocal
+REM endlocal
