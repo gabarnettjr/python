b00ef5662fb3600714bb7aff3cceb9f27d6ab0c8
gabarnettjr <gabarnettjr@gmail.com>
2020-04-05 17:11:22 -0600

    Added speedtest_getWeights() at the end of phs1.py which tests how fast the weights are generated.

--- batch/aliases.bat
+++ batch/aliases.bat
@@ -2,4 +2,8 @@
 
 set path=%path%;C:\Program Files\Notepad++
 
+set path=%path%;C:\Users\gabarnettjr\AppData\Local\Programs\Julia\Julia-1.4.0\bin
+
+set path=%path%;C:\Octave\Octave-5.2.0
+
 DOSKEY np=Notepad++ $*
---
+++ math1480/assignment5/solution.py
@@ -0,0 +1,7 @@
+## import the numpy library as np:
+import numpy as np
+##import the plotting library as plt:
+import matplotlib.pyplot as plt
+
+###########################################################################
+
