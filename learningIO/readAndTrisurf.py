b45e4ae7e39c7b3e225fe61984b16ef32dff0a97
gabarnettjr <gabarnettjr@gmail.com>
2020-04-17 21:35:07 -0600

    I moved a copy of this readAndTrisurf file to the main waveDisk folder over in the Julia repo, but I will leave this earlier version here for future reference, since it still shows a good example of how to read input from a text file and use it to plot figures and save them.

--- learningIO/readAndTrisurf.py
+++ learningIO/readAndTrisurf.py
@@ -1,73 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
-# from mpl_toolkits.mplot3d import Axes3D
+import sys
+
+#####################################################################
+
+# USER INPUT
+
+if len(sys.argv) > 1:
+    whatToPlot = sys.argv[1]
+else:
+    whatToPlot = "rho"
 
 # Define the base string which is where the files are located
 st = '../../julia/waveDiskPhsfd/results/'
 
-# Load the integer which is the total number of nodes
-with open(st + 'npts.txt') as f:
-    for line in f:
-        npts = np.int(line)
+# Set the contour levels
+if whatToPlot == "rho":
+    clevels = np.linspace(-.5,.5,41)
+elif (whatToPlot == "u") or (whatToPlot == "v"):
+    clevels = np.linspace(-1/3, 1/3, 41)
 
-# Load the integer which is the number of saves
-with open(st + 'nsaves.txt') as f:
-    for line in f:
-        nsaves = np.int(line)
+#####################################################################
+
+# Define the string used to load the variable you want to plot
+varst = st + whatToPlot
 
 # Load the x-coordinates of the points
-x = np.zeros((npts,))
-k = 0
+x = np.array([])
 with open(st + 'x.txt') as f:
     for line in f:
-        x[k] = np.float(line)
-        k = k+1
+        x = np.hstack((x, np.float(line)))
 
 # Load the y-coordinates of the points
-y = np.zeros((npts,))
-k = 0
+y = np.array([])
 with open(st + 'y.txt') as f:
     for line in f:
-        y[k] = np.float(line)
-        k = k+1
+        y = np.hstack((y, np.float(line)))
 
-# # Load the array of all times
-# t = np.array([])
-# with open(st + 't.txt') as f:
-#     for line in f:
-#         t = np.hstack((t, np.float(line)))
-
+# Get the triangular mesh for plotting the contours
 triang = mtri.Triangulation(x, y)
 
-rho = np.zeros((npts,))
+# Initialize the array to store the variable you want to plot
+var = np.zeros(np.shape(x))
 
-for i in range(nsaves):
+# Initialize the frame number
+frame = 0
 
-    k = 0
-    with open(st + "rho_{0:04d}.txt".format(i+1)) as f:
-        for line in f:
-            rho[k] = np.float(line)
-            k = k + 1
-    
-    fig = plt.figure(figsize = (12, 10))
-    ax = fig.add_subplot(111)
-    cs = ax.tricontourf(triang, rho, levels=np.linspace(-.5,.5,41))
-    fig.colorbar(cs)
-    plt.axis('equal')
-    plt.axis([-1,1,-1,1])
+# The main loop that plots and saves figures
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.plot_trisurf(tri, rho)
-    # ax.set_xlim3d(-1,1)
-    # ax.set_ylim3d(-1,1)
-    # ax.set_zlim3d(-1,1)
-    # plt.show()
+while True:
 
-    fig.savefig( '{0:04d}'.format(i) + '.png', bbox_inches = 'tight' )
+    try:
 
-    plt.close()
+        i = 0
+        with open(varst + "_{0:04d}.txt".format(frame)) as f:
+            for line in f:
+                var[i] = np.float(line)
+                i = i + 1
+
+    except:
+
+        break
+
+    else:
+
+        fig = plt.figure(figsize = (12, 10))
+        ax = fig.add_subplot(111)
+        cs = ax.tricontourf(triang, var, levels = clevels)
+        fig.colorbar(cs)
+        plt.axis('equal')
+        plt.axis([-1,1,-1,1])
+        
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # ax.plot_trisurf(tri, rho)
+        # ax.set_xlim3d(-1,1)
+        # ax.set_ylim3d(-1,1)
+        # ax.set_zlim3d(-1,1)
+        # plt.show()
+        
+        fig.savefig('{0:04d}'.format(frame) + '.png', bbox_inches = 'tight')
+        
+        plt.close()
+
+        frame = frame + 1
+
 
 
 
