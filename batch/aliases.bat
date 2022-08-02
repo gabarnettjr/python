560dc221787f82f16bf7303b99308ef1613b7ca9
gabarnettjr <gabarnettjr@gmail.com>
2022-08-01 21:38:26 -0600

    Update aliases.bat

--- batch/aliases.bat
+++ batch/aliases.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set path=%path%;C:\Program Files\Notepad++
+REM set path=%path%;C:\Program Files\Notepad++
 
-set path=%path%;C:\Users\gabarnettjr\AppData\Local\Programs\Julia\Julia-1.4.0\bin
+REM set path=%path%;C:\Users\gabar\AppData\Local\Programs\Julia\Julia-1.7.3\bin
 
-set path=%path%;C:\Octave\Octave-5.2.0
+REM set path=%path%;C:\Octave\Octave-5.2.0
 
-set path=%path%;C:\Program Files\Java\jdk-12.0.2\bin
+REM set path=%path%;C:\Program Files\Java\jdk-12.0.2\bin
 
 DOSKEY np=Notepad++ $*
